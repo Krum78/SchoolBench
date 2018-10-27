accb79bf5888521e841a1979785277c8cc574ed3
Oleksii Herasymov <krum78@gmail.com>
2018-10-27 20:35:32 +0300

    IS4 server

---
+++ SchoolBench.IS4/wwwroot/admin/assets/env.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Sam on 07/12/2016.
+ */
+
+var ENV = {
+        ApiUrl: 'https://localhost:44314',
+        UiUrl: 'https://localhost:44314',
+        AuthorityUrl: 'https://localhost:44314',
+        AddUserPassword: true,
+        CommunityEdition:true
+};
